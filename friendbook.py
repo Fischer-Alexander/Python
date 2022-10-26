f8b9c3adaab26ec2fe2222c3366c55cae2b6b7dd
Alexander <fischer.online@gmx.net>
2022-10-26 14:00:47 +0200

    Added listing functionality

--- friendbook.py
+++ friendbook.py
@@ -1,5 +1,10 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from collections.abc import Sequence
+
+from sqlalchemy import VARCHAR, Float, create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import declarative_base, sessionmaker
+
+from tabulate import tabulate
+
 
 
 # # # ###########################################
@@ -30,6 +35,8 @@ class Friend(Base):
     id = Column(Integer, primary_key=True)
     first_name = Column(String)
     last_name = Column(String)
+    email = Column(String)
+    tel = Column(VARCHAR)
 
     # Foreignkeys
     language_id = Column(Integer, ForeignKey("languages.id"))
@@ -38,24 +45,80 @@ class Friend(Base):
 def initialize_database():
     """
     Initializes the database and creates all tables.
+
     See more here: https://docs.sqlalchemy.org/en/14/orm/tutorial.html
     """
     Base.metadata.create_all(database)
 
 
+def show_menu():
+    """
+    Displays a menu.
+    """
+
+    MENU_TEXT = """
+    Menu: 
+    - (A)dd new Friend
+    - (L)ist all Friends (not implemented yet)
+    - (E)xit
+    """
+    print(MENU_TEXT)
+
+
+def get_users_menu_input():
+    menu_choice = input("Choose menue option: ")
+
+    if menu_choice == "A":
+        add_new_friend()
+    elif menu_choice == "L":
+        database_get_all_friends()
+    elif menu_choice == "E":
+        exit(1)
+    
+
+def add_new_friend():
+    """
+    Asks the user for the information about the new friend. 
+    Adds the friend to the database.
+    """
+    print("Einen neuen Freund hinzufügen")
+    first_name = input("First name\t:")
+    last_name = input("Last name\t:")
+    email = input("E-Mail\t:")
+    tel = input("Telephone\t:")
+    new_friend = Friend(first_name=first_name, last_name=last_name, email=email, tel=tel)
+    database_add_friend(new_friend)
+
+def list_all_friends():
+    database_get_all_friends()
+
+
 def database_add_friend(friend: Friend):
     """
     Database command to add a new friend.
+
+    ORM = Object Relational Mapper
     """
     session.add(friend)
+    session.commit()
 
 
 def database_get_all_friends():
     """
     Database command to get all friends.
+
+    `all_friends = session.query(Friend).all()` is a query call made with the sqlalchemy ORM. 
+    The SQL query that is made is: `SELECT * FROM friends;`
+
+    Alternative to this with raw sql:
+    `all_friends_raw = session.execute("SELECT * FROM friends;").fetchall()`
     """
     all_friends = session.query(Friend).all()
-    print(all_friends)
+
+
+    
+    for friend in all_friends:
+        print(tabulate([[friend.first_name, friend.last_name, friend.email, friend.tel ]], headers=['First Name', 'Last Name' , 'E-Mail', 'Telephone']))
 
 
 # # # ###########################################
@@ -64,9 +127,6 @@ def database_get_all_friends():
 if __name__ == "__main__":
     initialize_database()
 
-    # Example to add a new friend
-    new_friend = Friend(email="jack@example.com", first_name="Jack")
-    database_add_friend(new_friend)
-
-    # Example to list all friends
-    database_get_all_friends()
+    while True:
+        show_menu()
+        get_users_menu_input()
